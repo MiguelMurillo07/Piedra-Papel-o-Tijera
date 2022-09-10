0f9b9d88def46cf9aaa60a32332a6ded8901a1ce
MiguelMurillo07 <jose2224644@correo.uis.edu.co>
2022-09-10 09:46:40 -0500

    Inicio Juego

---
+++ piedra_papel_tijera.py
@@ -0,0 +1,31 @@
+# Juego Piedra, Papel o Tijera con número aleatorio
+
+
+print("-------------------------------")
+print("-----Piedra, Papel o Tijera----")
+print("-------------------------------")
+
+#input
+
+a = int(input)
+b = 
+c = 
+
+z = int(input("Jugador real digita su valor:"))
+num_maq = random_randint(1, 3)
+print(" \nDame el valor que quieres tener en el juego: \n")
+# Se despliega el menú de opciones para jugar:
+print("1. Sacar Piedra.")
+print("2. Sacar Papel.")
+print("3. Sacar Tijera.")
+
+opcion = int(input("Dame el lugar que quieres tener para jugar: "))
+
+if(opcion == 1):
+
+
+
+
+# Processing
+
+
