e4e86e1bf2c597ead7f9f6a8938e2337a9fc9039
MiguelMurillo07 <jose2224644@correo.uis.edu.co>
2022-09-13 21:27:29 -0500

    Fin Juego

--- piedra_papel_tijera.py
+++ piedra_papel_tijera.py
@@ -1,31 +1,43 @@
-# Juego Piedra, Papel o Tijera con número aleatorio
+# Juego Piedra, Papel o Tijera con opción aleatoria
 
+import random
 
 print("-------------------------------")
 print("-----Piedra, Papel o Tijera----")
 print("-------------------------------")
 
-#input
-
-a = int(input)
-b = 
-c = 
-
-z = int(input("Jugador real digita su valor:"))
-num_maq = random_randint(1, 3)
-print(" \nDame el valor que quieres tener en el juego: \n")
-# Se despliega el menú de opciones para jugar:
+# input
+print(" \nDime el valor que quieres tener en el juego: \n")
+# Se despliega el menú de opciones para empezar a jugar:
 print("1. Sacar Piedra.")
 print("2. Sacar Papel.")
 print("3. Sacar Tijera.")
+num_maq = random.randint(1, 3)
 
 opcion = int(input("Dame el lugar que quieres tener para jugar: "))
 
-if(opcion == 1):
+# Processing and output
 
+if(opcion) ==1:
+    if num_maq==1:
+        print("La máquina sacó Piedra, lo que quiere decir que hubo empate!.")
+    elif num_maq==2:
+        print("La máquina sacó Papel, lo que quiere decir que perdiste!.")
+    elif num_maq==3:
+        print("La máquina sacó Tijera, lo que quiere decir que ganaste!.")
 
+if(opcion) ==2:
+    if num_maq==1:
+        print("La máquina sacó Piedra, lo que quiere decir que ganaste!. ")
+    elif num_maq==2:
+        print("La máquina sacó Papel, lo que quiere decir que hubo empate!.")
+    elif num_maq==3:
+        print("La máquina sacó Tijera, lo que quiere decir perdiste!.")
 
-
-# Processing
-
-
+if(opcion) ==3:
+    if num_maq==1:
+        print("La máquina sacó Piedra, lo que quiere decir que perdiste!.")
+    elif num_maq==2:
+        print("La máquina sacó Papel, lo que quiere decir que ganaste!.")
+    elif num_maq==3:
+        print("La máquina sacó Tijera, lo que quiere decir que hubo empate!.")
